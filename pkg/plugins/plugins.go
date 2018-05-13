c8e00ce375a1c94e7bc92c742b1c9b11c929d5a2
Jacobious52 <jacobgonzalez5252@gmail.com>
2018-05-14 02:49:42 +0930

    added planned plugins comments

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -5,4 +5,14 @@ package plugins
 func (p *PluginManager) RegisterAll() {
 	p.RegisterPlugin("help", new(help))
 	p.RegisterPlugin("fav", new(favorite))
+	// planned
+	// --------
+	// freq emojis
+	// sentiment
+	// top 10 words
+	// most messages (model change)
+	// top swears
+	// top puncuation
+	// longest messages (model change)
+	// shortest messages (model change)
 }
